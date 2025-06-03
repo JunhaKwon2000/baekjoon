1617b65de57a460a4afc403ea43bf87347d4085c
JunhaKwon2000 <junharoket@naver.com>
2025-06-03 15:59:06 +0900

    업데이트
    10828

--- baekjoon/src/pkg_10828/Main.java
+++ baekjoon/src/pkg_10828/Main.java
@@ -1,7 +1,9 @@
-package pkg_toy;
+package pkg_10828;
 
-import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
+// 백준에서 그냥 Exception 타입이 아닌 특정 Exception 을 Catch 하면 시간초과가 뜸 (아마 모든 종류의 에외가 아닌 그 특정 예외만 잡아야해서 더 느린듯..)
+// 특정 예외를 잡아도 되지만, 시간 초과 이슈로 코드에서는 그냥 Exception 으로 통일함
+// import java.util.EmptyStackException;
+// import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -21,7 +23,7 @@ public class Main {
                 try {
                     int pop = stack.pop();
                     System.out.println(pop);
-                } catch (EmptyStackException e) {
+                } catch (Exception e) {
                     System.out.println(-1);
                 }
             } else if (choice.equals("size")) {
@@ -32,7 +34,7 @@ public class Main {
             } else if (choice.equals("top")) {
                 try {
                     System.out.println(stack.lastElement());
-                } catch (NoSuchElementException e) {
+                } catch (Exception e) {
                     System.out.println(-1);
                 }
             }
@@ -44,3 +46,4 @@ public class Main {
 
     }
 }
+
