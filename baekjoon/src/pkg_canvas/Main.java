3c47339cb832042c32ba397a2ef3ec67611b4c93
Junha Kwon <junharoket@naver.com>
2025-06-17 12:26:51 +0900

    Update
    백준 인텔리제이 이식

--- baekjoon/src/pkg_canvas/Main.java
+++ baekjoon/src/pkg_canvas/Main.java
@@ -1,7 +1,5 @@
 package pkg_canvas;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,15 +7,6 @@ public class Main {
         
         Scanner sc =  new Scanner(System.in);
 
-        String line1 = sc.nextLine();
-        String[] l1 = line1.split("");
-        String line2 = sc.nextLine();
-        String[] l2 = line2.split("");
-
-        List<String> result = new ArrayList<>();
-        if (((l1[0] + l1[1] + l2[1] + l2[0]).equals("HEPC")) || ((l1[0] + l2[0] + l2[1] + l1[1]).equals("HEPC"))) result.add("YES");
-        
-        
         sc.close();
 
     }
